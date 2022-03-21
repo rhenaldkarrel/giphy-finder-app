b6a6646990bf380ff7dffbdbd07e1c2b2078c392
Rhenald Karrel <renaldkarel@gmail.com>
2022-03-21 15:34:37 +0700

    Finishing Hands-on

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<form action=''>
+					<input type='text' placeholder='Find a giphy...' />
+					<button>Search</button>
+				</form>
+				<div className='result'>
+					<img
+						src='https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif'
+						alt='Result'
+					/>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
